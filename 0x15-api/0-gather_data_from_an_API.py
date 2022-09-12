84bf831efa3624ea12fe1679fb8c3448fc1c3905
rominamarmontel <romi.varnier@gmail.com>
2022-09-12 13:47:23 +0000

    1st

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""" Write a Python script that, using this REST API,
+for a given employee ID, returns information """
+
+from sys import argv
+import requests
+import json
+
+
+if __name__ == "__main__":
+    userId = int(argv[1])
+    user = ('https://jsonplaceholder.typicode.com/users/{}'.format(userId))
+    todo = ('{}/todos/'.format(user))
+    json_user = requests.get(user).json()
+    json_todo = requests.get(todo).json()
+    completed_tasks = []
+
+    for task in json_todo:
+        if task.get('completed') is True:
+            completed_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(json_user.get('name'), len(completed_tasks), len(json_todo)))
+    print("\n".join("\t {}".format(task) for task in completed_tasks))
