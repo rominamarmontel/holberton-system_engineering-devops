b63338a89fd182b640d463fd782880c2cfdfb151
rominamarmontel <romi.varnier@gmail.com>
2022-10-06 14:08:24 +0000

    change the number

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 exec { 'Update nginx':
     path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-    command => 'sed -i s/15/4096/ /etc/default/nginx'
+    command => 'sed -i s/15/2000/ /etc/default/nginx'
 }
 
 exec { 'Restart nginx':
