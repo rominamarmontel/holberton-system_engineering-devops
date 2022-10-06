e64707e86173ca65f344805db97996e4e11bb9e0
rominamarmontel <romi.varnier@gmail.com>
2022-10-06 13:59:34 +0000

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# change the ulimit default value to fix the requests failed
+
+exec { 'Update nginx':
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+    command => 'sed -i "s/15/4096/" /etc/default/nginx'
+}
+
+exec { 'Restart nginx':
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+    command => 'sudo service nginx restart'
+}
